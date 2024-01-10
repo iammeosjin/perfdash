c050e8969e44a895b3635c49bb78383c170d2099
meosjin <iammeosjin@gmail.com>
2024-01-10 20:40:10 +0800

    feat: test sqlite

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -2,28 +2,31 @@
 // This file SHOULD be checked into source version control.
 // This file is automatically updated during development when running `dev.ts`.
 
-import * as $_404 from "./routes/_404.tsx";
-import * as $_app from "./routes/_app.tsx";
-import * as $api_joke from "./routes/api/joke.ts";
-import * as $greet_name_ from "./routes/greet/[name].tsx";
-import * as $index from "./routes/index.tsx";
-import * as $version from "./routes/version.ts";
-import * as $Counter from "./islands/Counter.tsx";
-import { type Manifest } from "$fresh/server.ts";
+import * as $_404 from './routes/_404.tsx';
+import * as $_app from './routes/_app.tsx';
+import * as $api_joke from './routes/api/joke.ts';
+import * as $backend_team_year_month_day_ from './routes/backend/[team]/[year]/[month]/[day].tsx';
+import * as $greet_name_ from './routes/greet/[name].tsx';
+import * as $index from './routes/index.tsx';
+import * as $version from './routes/version.ts';
+import * as $Counter from './islands/Counter.tsx';
+import { type Manifest } from '$fresh/server.ts';
 
 const manifest = {
-  routes: {
-    "./routes/_404.tsx": $_404,
-    "./routes/_app.tsx": $_app,
-    "./routes/api/joke.ts": $api_joke,
-    "./routes/greet/[name].tsx": $greet_name_,
-    "./routes/index.tsx": $index,
-    "./routes/version.ts": $version,
-  },
-  islands: {
-    "./islands/Counter.tsx": $Counter,
-  },
-  baseUrl: import.meta.url,
+	routes: {
+		'./routes/_404.tsx': $_404,
+		'./routes/_app.tsx': $_app,
+		'./routes/api/joke.ts': $api_joke,
+		'./routes/backend/[team]/[year]/[month]/[day].tsx':
+			$backend_team_year_month_day_,
+		'./routes/greet/[name].tsx': $greet_name_,
+		'./routes/index.tsx': $index,
+		'./routes/version.ts': $version,
+	},
+	islands: {
+		'./islands/Counter.tsx': $Counter,
+	},
+	baseUrl: import.meta.url,
 } satisfies Manifest;
 
 export default manifest;
--- main.ts
+++ main.ts
@@ -4,10 +4,18 @@
 /// <reference lib="dom.asynciterable" />
 /// <reference lib="deno.ns" />
 
-import "$std/dotenv/load.ts";
+import '$std/dotenv/load.ts';
 
-import { start } from "$fresh/server.ts";
-import manifest from "./fresh.gen.ts";
-import config from "./fresh.config.ts";
+import { start } from '$fresh/server.ts';
+import manifest from './fresh.gen.ts';
+import config from './fresh.config.ts';
+import sqlite from './libs/sqlite.ts';
+
+sqlite.execute(`
+CREATE TABLE IF NOT EXISTS people (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT
+)
+`);
 
 await start(manifest, config);
