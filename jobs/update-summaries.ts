1f646d049c0d0db914d94e5bdc3e446cd5450942
meosjin <iammeosjin@gmail.com>
2024-01-15 19:05:07 +0800

    feat: add time process

--- jobs/update-summaries.ts
+++ jobs/update-summaries.ts
@@ -6,6 +6,7 @@ import fetchJiraTasks from './fetch-jira-tasks.ts';
 
 export default async function updateSummaries() {
 	const id = ['update-summaries'];
+	console.time('update-summaries');
 	const job = await JobModel.get(id);
 	console.log('job', job);
 
@@ -13,33 +14,38 @@ export default async function updateSummaries() {
 
 	if (job.status !== JobStatus.READY) return;
 
+	console.time('github-process');
 	await JobModel.insert({
 		...job,
 		status: JobStatus.PROCESSING,
 		body: { api: ['github'] },
 	});
-
 	console.log('processing github job');
 	await fetchGithubPullRequests([Team.NEXIUX, Team.OPEXA]);
+	console.timeEnd('github-process');
+
+	console.time('jira-process');
 	await JobModel.insert({
 		...job,
 		status: JobStatus.PROCESSING,
 		body: { api: ['github', 'jira'] },
 	});
-
 	console.log('processing jira job');
 	await fetchJiraTasks([Team.NEXIUX]);
+	console.timeEnd('jira-process');
 
+	console.time('clickup-process');
 	await JobModel.insert({
 		...job,
 		status: JobStatus.PROCESSING,
 		body: { api: ['github', 'jira', 'clickup'] },
 	});
-
 	console.log('processing clickup job');
 	await fetchClickupTasks([Team.OPEXA]);
+	console.timeEnd('clickup-process');
 
 	console.log('processing done');
+	console.timeEnd('update-summaries');
 
 	await JobModel.insert({
 		...job,
